ebd695942248cdfc04d0b940f8fdd47a6e6dfe32
LAPTOP-5K218H50\Supercom - 1 <ritthirach.kongjarit@mail.kmutt.ac.th>
2021-05-03 03:33:55 +0700

    ++Finally found a way to do it, God's forbid

---
+++ Pascal's Triangle Maker/Pascal's Triangle Maker/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Pascal_s_Triangle_Maker
+{
+    class Program
+    {
+        public static void Main()
+        {
+            int row;
+            row = int.Parse(Console.ReadLine());
+            while (row < 0)
+            {
+                Console.WriteLine("Invalid Pascal’s triangle row number.");
+                row = int.Parse(Console.ReadLine());
+            }
+            MakePTriangle(row);
+        }
+
+        public static void MakePTriangle(int row)
+        {
+            for (int amount = 1;
+                    amount <= row; amount++)
+            {
+
+                int C = 1;
+                for (int i = 1; i <= amount; i++)
+                {
+                    Console.Write(C + " ");
+                    C = C * (amount - i) / i;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
+
